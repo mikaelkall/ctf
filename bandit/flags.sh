4ce3d7f927afdc5a6fda3f1ff4658351f0555c9a
Mikael Kall <Mikael.Kall@kindredplc.com>
2017-12-15 21:48:51 +0100

    Added shellscript that get all flags.sh

---
+++ bandit/flags.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Gets all flags
+
+SCRIPT_DIR=$(dirname $0)
+cd ${SCRIPT_DIR}
+
+# Colors
+RESET=$(tput sgr0)
+RED="\e[91m"
+GREEN="\e[92m"
+
+for solver in $(ls -1 level{0..23}/solution.py | sort -V); 
+do
+    output=$(./${solver} 2>/dev/null)
+    level=$(dirname $solver)  
+    level=${level/level/bandit}
+ 
+    IFS=$'\n'
+    for s in ${output};
+    do
+	if [[ "${s}" =~ "Flag =" ]]; then
+	    printf "%s : ${GREEN}%s${RESET}\n" "${level}" "${s#*=}"
+	fi 
+    done
+done
