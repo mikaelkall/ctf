00641ae55f04678932a4fddc391164fba97fd171
Mikael Kall <Mikael.Kall@kindredgroup.com>
2018-02-05 10:07:09 +0100

    Updated filename to make more sence

--- leviathan/flags.sh
+++ leviathan/flags.sh
@@ -9,17 +9,16 @@ RESET=$(tput sgr0)
 RED="\e[91m"
 GREEN="\e[92m"
 
-for solver in $(ls -1 *.py | sort -V); 
+for solver in $(ls -1 *.py | sort -V);
 do
     output=$(./${solver} 2>/dev/null)
     level="${solver::-3}"
-    level=${level/level/leviathan}
 
     IFS=$'\n'
     for s in ${output};
     do
 	if [[ "${s}" =~ "Flag =" ]]; then
 	    printf "%s : ${GREEN}%s${RESET}\n" "${level}" "${s#*=}"
-	fi 
+	fi
     done
 done
