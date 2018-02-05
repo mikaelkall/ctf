24e357b7a7149a96626decfb14ef6564438f7f9e
Mikael Kall <Mikael.Kall@kindredgroup.com>
2018-02-05 10:05:19 +0100

    Updated directory name to bandit to make more sence

--- bandit/flags.sh
+++ bandit/flags.sh
@@ -9,17 +9,16 @@ RESET=$(tput sgr0)
 RED="\e[91m"
 GREEN="\e[92m"
 
-for solver in $(ls -1 level{0..23}/solution.py | sort -V); 
+for solver in $(ls -1 bandit{0..23}/solution.py | sort -V);
 do
     output=$(./${solver} 2>/dev/null)
-    level=$(dirname $solver)  
-    level=${level/level/bandit}
- 
+    level=$(dirname $solver)
+
     IFS=$'\n'
     for s in ${output};
     do
 	if [[ "${s}" =~ "Flag =" ]]; then
 	    printf "%s : ${GREEN}%s${RESET}\n" "${level}" "${s#*=}"
-	fi 
+	fi
     done
 done
